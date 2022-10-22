89634fe40e46e89831d8e0400a41c023bc68ad9b
masterdd15 <masterdd15@gmail.com>
2022-10-22 17:07:47 -0400

    Finished Base Player Controller
    I finished the tutorial. The PlayerController now has mouse input and movement input. The next step is to add the controls for the umbrella mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform playerCamera = null;
     [SerializeField] float mouseSensitivity = 3.5f;
-
-    float cameraPitch = 0.0f;
+    [SerializeField] float walkSpeed = 6.0f;
+    [SerializeField] float gravity = -13.0f;
+    [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
 
     [SerializeField] bool lockCursor = true;
 
+    float cameraPitch = 0.0f;
+    float velocityY = 0.0f;
+    CharacterController controller = null;
+
+    //Stores for our movement
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
+
+    //Stores for mouse input
+    Vector2 currentMouseDelta = Vector2.zero;
+    Vector2 currentMouseDeltaVelocity = Vector2.zero;
+
+    private void Awake()
+    {
+        //I'm setting the controller in awake. Tutorial put it in start.
+        controller = GetComponent<CharacterController>(); 
+    }
     void Start()
     {
         if(lockCursor)
@@ -23,17 +42,41 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         UpdateMouseLook();
+        UpdateMovement();
     }
 
     void UpdateMouseLook()
     {
-        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        cameraPitch -= mouseDelta.y * mouseSensitivity; //We subtract to invert the delta
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity; //We subtract to invert the delta
         cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f); //Clamp camera
 
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
 
-        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+    }
+
+    void UpdateMovement()
+    {
+        //This is the vector we are trying to get to, without any smoothing
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize(); //Keeps all movement directions to 1, even if moving NE, SW, etc.
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if(controller.isGrounded)
+        {
+            velocityY = 0.0f;
+        }
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed
+            + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
