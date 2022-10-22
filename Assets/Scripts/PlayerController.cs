0e1306bf6166386fb9ba0eeee98c55cca40812a9
masterdd15 <masterdd15@gmail.com>
2022-10-22 16:41:17 -0400

    Implemented Camera look
    The Player Movement script now has a full range of camera movement for the player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+
+    float cameraPitch = 0.0f;
+
+    [SerializeField] bool lockCursor = true;
+
+    void Start()
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        UpdateMouseLook();
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        cameraPitch -= mouseDelta.y * mouseSensitivity; //We subtract to invert the delta
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f); //Clamp camera
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+    }
+}
