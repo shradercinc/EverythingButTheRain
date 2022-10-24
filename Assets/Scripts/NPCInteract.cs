9efbd256c2a8bc337639f452e6ccc04ab1f1ca84
shradercinc <82776089+shradercinc@users.noreply.github.com>
2022-10-24 04:13:45 -0400

    Added NPC interaction mechanism and placeholder voice lines

--- Assets/Scripts/NPCInteract.cs
+++ Assets/Scripts/NPCInteract.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NPCInteract : MonoBehaviour
 {
+    float state = 0;
+    public bool happy = false;
+    public float stateSet = 0;
+    public AudioClip[] clips;
+    private AudioSource aud;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,31 @@ public class NPCInteract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (state == 0)
+        {
+            state = stateSet;
+        }
+        //play unhappy splash
+        if (state == 1)
+        {
+            state = 1.1f;
+            aud.PlayOneShot(clips[0]);
+        }
+        //play 
+        if (state == 2)
+        {
+            state = 2.1f;
+            aud.PlayOneShot(clips[1]);
+        }
+        if (state == 3)
+        {
+            state = 3.1f;
+            aud.PlayOneShot(clips[2]);
+        }
+        if (state != 0 && aud.isPlaying == false) 
+        {
+            state = 0;
+        }
+        stateSet = 0;
     }
 }
--- Assets/Scripts/RainColide.cs
+++ Assets/Scripts/RainColide.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class RainColide : MonoBehaviour
 {
+    private Transform pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos = GetComponent<Transform>();
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<NPCInteract>())
+        {
+            if (other.gameObject.GetComponent<NPCInteract>().happy == false)
+            {
+                other.gameObject.GetComponent<NPCInteract>().stateSet = 1;
+            }
+            else
+            {
+                other.gameObject.GetComponent<NPCInteract>().stateSet = 3;
+            }
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
