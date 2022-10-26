55638c6a5f37da4f56f3c7dda1a2ab711bda27be
masterdd15 <masterdd15@gmail.com>
2022-10-26 16:14:19 -0400

    Working on Talking NPC
    Trying to implement Nicole's NPC interact code to work with the following NPC

--- Assets/Scripts/NPCFollow.cs
+++ Assets/Scripts/NPCFollow.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCFollow : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent npc;
     [SerializeField] Transform player;
 
     private void Awake()
     {
+        npc = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     // Start is called before the first frame update
@@ -19,6 +22,9 @@ public class NPCFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This makes the NPC always stare at the player
         transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+
+        npc.SetDestination(player.position);
     }
 }
