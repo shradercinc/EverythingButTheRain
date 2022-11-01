6c731b79692800b8bd7464af42d3c45292f2b838
masterdd15 <masterdd15@gmail.com>
2022-11-01 14:47:13 -0400

    Creating Tutorial Event
    I'm starting to create the tutorial. I'm creating the elements in a second scene before I try and implement them into the main one, just because I'm worried about breaking something

---
+++ Assets/Scripts/Event_Tutorial.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine("TutorialSetup");
+        }
+    }
+
+    IEnumerator TutorialSetup()
+    {
+        Debug.Log("WAIT!");
+        yield return new WaitForSeconds(2f);
+        Debug.Log("DONE");
+    }
+
+    //This object is controlling the tutorial event for the player
+
+    //The player will be blocked from progressing down the street, due to a crowd in front of the dorm
+
+    //The player will hear an voice clip play, and have UI appear which shows the player how to use their umbrella
+}
