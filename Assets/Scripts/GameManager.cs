e06bde14593dc2de8acf9e1ea4153c3724c3440d
masterdd15 <masterdd15@gmail.com>
2022-11-06 22:22:05 -0500

    Created a Scene Manager with Main Menu
    Started the creation of the GameManager by working on a main menu and scene managment. We have a persistant scene, which all scenes will load on top of. This will only occur if the scene is the first in the build, and should be irrelivent while testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public GameObject loadingScreen;
 
     public GameState State;
 
@@ -15,7 +16,12 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         Instance = this;
-        SceneManager.LoadSceneAsync((int)SceneIndexes.TITLE_SCREEN, LoadSceneMode.Additive);
+        int index = SceneManager.GetActiveScene().buildIndex;
+        //Here, we check to see if we are in our manager. If not, we let the scene run normally
+        if (index == (int)SceneIndexes.MANAGER)
+        {
+            SceneManager.LoadSceneAsync((int)SceneIndexes.TITLE_SCREEN, LoadSceneMode.Additive);
+        }
     }
 
     void Start()
@@ -23,10 +29,28 @@ public class GameManager : MonoBehaviour
 
     }
 
+    List<AsyncOperation> scenesLoading = new List<AsyncOperation>();
+
     public void LoadScene()
     {
-        SceneManager.UnloadSceneAsync((int)SceneIndexes.TITLE_SCREEN);
-        SceneManager.LoadSceneAsync((int)SceneIndexes.DAY_1, LoadSceneMode.Additive);
+        loadingScreen.SetActive(true);
+        scenesLoading.Add(SceneManager.UnloadSceneAsync((int)SceneIndexes.TITLE_SCREEN));
+        scenesLoading.Add(SceneManager.LoadSceneAsync((int)SceneIndexes.DAY_1, LoadSceneMode.Additive));
+
+        StartCoroutine(GetSceneLoadProgress());
+    }
+
+    public IEnumerator GetSceneLoadProgress()
+    {
+        for(int i = 0; i < scenesLoading.Count; i++)
+        {
+            while(!scenesLoading[i].isDone)
+            {
+                yield return null;
+            }
+        }
+
+        loadingScreen.gameObject.SetActive(false);
     }
 
     public enum GameState
